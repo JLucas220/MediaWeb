b8833682a6244c0b2d90ac1f5fe7cc36cb1d48c3
MigMarques <miguel.marques@mediaweb.pt>
2022-03-18 16:42:52 +0000

    fix project

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,14 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglifycss = require('gulp-uglifycss');
- 
-gulp.task('sass', function (){
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-    return gulp.src('./sass/**/*.scss')
-    .pipe(sass().once('error', sass.logErros))
+gulp.task('buildStyles', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('css', function () {
-  gulp.src('./styles/**/*.css')
-    .pipe(uglifycss({
-      "uglyComments": true
-    }))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('run'['sass', 'css']);
-
 gulp.task('watch', function(){
-
-    gulp.watch('./sass/*.sass', ['sass']);
-    gulp.watch('./css/*.css', ['css']);
+  gulp.watch('./scss/**/*.scss', gulp.series('buildStyles'));
 });
 
-gulp.task('defailt', ['run', 'watch']);
+gulp.task('default', gulp.series('watch'));
